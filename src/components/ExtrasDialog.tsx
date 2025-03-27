b8a72917c36d54e98d712ddd9a287bc89cca74fc
John <johnlouisstephenson@gmail.com>
2025-03-27 14:56:07 +0000

    add: react hook form for selection count

--- src/components/ExtrasDialog.tsx
+++ src/components/ExtrasDialog.tsx
@@ -1,3 +1,4 @@
+import { useFormContext } from "react-hook-form";
 import { formatPrice } from "@/utils/formatters";
 import { Extra } from "@/utils/types/menu";
 
@@ -6,12 +7,18 @@ type ExtrasDialogProps = {
 };
 
 export function ExtrasDialog({ extra }: ExtrasDialogProps) {
+  const { register } = useFormContext();
+
   return (
     <div className="flex items-center justify-between">
       <label htmlFor={extra.name}>
         {`${extra.name} (+${formatPrice(extra.price)})`}
       </label>
-      <input id={extra.name} type="checkbox" />
+      <input
+        {...register(`extras.extra_${extra.id}`)}
+        id={extra.name}
+        type="checkbox"
+      />
     </div>
   );
 }
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,3 +1,4 @@
+import { useForm, FormProvider } from "react-hook-form";
 import { Button } from "./ui/button";
 import {
   Dialog,
@@ -8,69 +9,86 @@ import {
   DialogFooter,
 } from "./ui/dialog";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "./ui/card";
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import { DisplayItem, TransformedMenuItems } from "@/utils/types/menu";
 import { ExtrasDialog } from "./ExtrasDialog";
 import { formatPrice } from "@/utils/formatters";
-
 
 type MenuItemProps = {
   item: TransformedMenuItems;
 };
 
 export function MenuItem({ item }: MenuItemProps) {
+  const methods = useForm();
+
+  const { watch } = methods;
+
+  const formValues = watch("extras") ?? {};
+
+
   return item.displayItems.map((displayItem: DisplayItem) => (
-    <Card key={displayItem.id}>
-      <CardHeader>
-        <CardTitle>{displayItem.name}</CardTitle>
-        <CardDescription>{formatPrice(displayItem.price)}</CardDescription>
-      </CardHeader>
-      <CardContent className="self-center">
-        <img
-          src={displayItem.imageUrl}
-          alt={displayItem.name}
-          className="aspect-square w-full max-w-28 self-center rounded-lg object-cover object-center"
-        />
-      </CardContent>
-      <CardFooter className="w-full flex-col gap-3 self-center">
-        <CardDescription className="max-w-[20em] self-center text-center">
-          {displayItem.description}
-        </CardDescription>
-        {item.extras.length === 0 ? (
-          <Button className="self-end hover:cursor-pointer">Add</Button>
-        ) : (
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button className="self-end hover:cursor-pointer">Add</Button>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>{displayItem.name}</DialogTitle>
-              </DialogHeader>
-              <form className="grid gap-4">
-                <h4 className="font-medium">Extras:</h4>
-                <div className="grid gap-2">
-                  {item.extras.map((extra) => (
-                    <ExtrasDialog key={extra.id} extra={extra} />
-                  ))}
-                </div>
-                <p>Maximum amount of extras, {displayItem.maxSelectCount}, reached</p>
-              </form>
-              <DialogFooter className="w-full">
-                <Button className="w-full" type="submit">
-                  Add for {formatPrice(displayItem.price)}
-                </Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-        )}
-      </CardFooter>
-    </Card>
+    <FormProvider {...methods}>
+      <Card key={displayItem.id}>
+        <CardHeader>
+          <CardTitle>{displayItem.name}</CardTitle>
+          <CardDescription>{formatPrice(displayItem.price)}</CardDescription>
+        </CardHeader>
+        <CardContent className="self-center">
+          <img
+            src={displayItem.imageUrl}
+            alt={displayItem.name}
+            className="aspect-square w-full max-w-28 self-center rounded-lg object-cover object-center"
+          />
+        </CardContent>
+        <CardFooter className="w-full flex-col gap-3 self-center">
+          <CardDescription className="max-w-[20em] self-center text-center">
+            {displayItem.description}
+          </CardDescription>
+          {item.extras.length === 0 ? (
+            <Button className="self-end hover:cursor-pointer">Add</Button>
+          ) : (
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className="self-end hover:cursor-pointer">Add</Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>{displayItem.name}</DialogTitle>
+                </DialogHeader>
+                <form className="grid gap-4">
+                  <h4 className="font-medium">Extras:</h4>
+                  <div className="grid gap-2">
+                    {item.extras.map((extra) => (
+                      <ExtrasDialog
+                        key={extra.id}
+                        extra={extra}
+                      />
+                    ))}
+                  </div>
+                  {Object.values(formValues).filter((value) => value !== false).length ===
+                    displayItem.maxSelectCount && (
+                    <p className="text-red-700">
+                      Maximum number of extras ({displayItem.maxSelectCount})
+                      reached
+                    </p>
+                  )}
+                </form>
+                <DialogFooter className="w-full">
+                  <Button className="w-full" type="submit">
+                    Add for {formatPrice(displayItem.price)}
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          )}
+        </CardFooter>
+      </Card>
+    </FormProvider>
   ));
 }
