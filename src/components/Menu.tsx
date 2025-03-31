99fb78687e320e12b19f22178162596a22011e59
John <johnlouisstephenson@gmail.com>
2025-03-31 12:19:21 +0100

    refactor: custom hook for query, caching on github action

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,24 +3,23 @@ import { transformMenu } from "@/utils/transformMenu";
 import { MenuItem } from "./MenuItem";
 
 export function Menu() {
-  const {
-    data: menu,
-    isPending,
-    isError,
-    error,
-  } = useQuery({
-    queryKey: ["menu"],
-    queryFn: async () => {
-      const response = await fetch(
-        "https://menus.flipdish.co/prod/16798/e6220da2-c34a-4ea2-bb51-a3e190fc5f08.json",
-      );
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return await response.json();
-    },
-    select: transformMenu,
-  });
+  function useMenu() {
+    return useQuery({
+      queryKey: ["menu"],
+      queryFn: async () => {
+        const response = await fetch(
+          "https://menus.flipdish.co/prod/16798/e6220da2-c34a-4ea2-bb51-a3e190fc5f08.json",
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return await response.json();
+      },
+      select: transformMenu,
+    });
+  }
+
+  const { data: menu, isPending, isError, error } = useMenu();
 
   return (
     <main className="mx-auto mt-6 max-w-7xl space-y-8 px-2">
